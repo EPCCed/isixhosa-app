b1db8c3de5e0562bf604de1914226135e5108502
lucyno <86615477+lucyno@users.noreply.github.com>
2025-01-22 15:07:31 +0000

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,56 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+  
+})
+var myTrack =new Audio("../assets/AudioTest.m4a")
+function play(event) {
+  //alert(`lang= ${lang.value}`);
+  myTrack.play()
+  //count.value="V"
+  //alert(`lang= ${lang.value}`);
+  } 
+</script>
+
+<template>
+
+  <div class="greetings">
+    <img alt="isiXhosa" class="logo" src="../assets/isiXhosaLogo.png" width="400" height="400"/>
+    <h1 class="darkred">IPHEPHA LEMIBUZO</h1>
+    <h1 class="darkred">LEMPILO YESIGUME</h1>
+    <h3>PATIENT HEALTH QUESTIONNAIRE</h3>
+    <br><br><br>
+   <img alt="isiXhosa" src="../assets/UoELogo.png" width="120" height="30" />            
+   <img alt="isiXhosa" src="../assets/StellenboschLogo.png" width="120" height="30" />
+
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 700;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-weight:  500;
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,108 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/email',
+      name: 'email',
+      component: () => import('../views/EmailView.vue')
+    },
+    {
+      path: '/ptid',
+      name: 'ptid',
+      component: () => import('../views/PtIDView.vue')
+    },
+    {
+      path: '/language',
+      name: 'language',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LanguageView.vue')
+    },
+    {
+      path: '/audioinfo/:lang',
+      name: 'audioinfo',
+      component: () => import('../views/AudioInfoView.vue')
+    },
+    {
+      path: '/disclaimer/:lang',
+      name: 'disclaimer',
+      component: () => import('../views/DisclaimerView.vue')
+    },
+    {
+      path: '/disclaimer2/:lang',
+      name: 'disclaimer2',
+      component: () => import('../views/Disclaimer2View.vue')
+    },
+    {
+      path: '/disclaimer3/:lang',
+      name: 'disclaimer3',
+      component: () => import('../views/Disclaimer3View.vue')
+    },
+    {
+      path: '/qintro/:lang',
+      name: 'qintro',
+      component: () => import('../views/QIntroView.vue')
+    },
+    {
+      path: '/question/:lang/:total/:qnumber',
+      name: 'question',
+      component: () => import('../views/QuestionView.vue')
+    },
+    {
+      path: '/extraquestion/:lang/:total',
+      name: 'extraquestion',
+      component: () => import('../views/Q10View.vue')
+    },
+    {
+      path: '/complete/:lang/:total',
+      name: 'complete',
+      component: () => import('../views/CompleteView.vue')
+    },
+    {
+      path: '/score/:lang/:total',
+      name: 'score',
+      component: () => import('../views/ScoreView.vue')
+    },
+    {
+      path: '/Q1/:lang/:total',
+      name: 'Q1',
+      component: () => import('../views/Q1View.vue')
+    },
+    {
+      path: '/Q2/:lang/:total',
+      name: 'Q2',
+      component: () => import('../views/Q2View.vue')
+    },
+    {
+      path: '/aboutapp',
+      name: 'aboutapp',
+      component: () => import('../views/AboutAppView.vue')
+    },
+    {
+      path: '/acknowl',
+      name: 'acknowl',
+      component: () => import('../views/AcknowlView.vue')
+    },
+    {
+      path: '/about/:lang',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/forminput',
+      name: 'forminput',
+      component: () => import('../views/FormInput.vue')
+    },
+  ]
+})
+
+export default router
